8f18e4662f9e8dd94b79c3adc015e9c60ee8b466
Troy Chryssos <github email address>
2016-10-31 13:43:28 -0400

    created basic react structure

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Link} from 'react-router'
+
+export default function Header(props){
+  return(
+    <div className="header">
+      <Link to="/"><h1> BUM ME OUT </h1>
+    </div>
+  )
+}
