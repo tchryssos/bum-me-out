7200f7e4d0de2df72bc0cc026881a765887fa665
Troy Chryssos <github email address>
2016-12-07 15:16:13 -0500

    removed useless constructor in sadness display

--- src/components/sadness-display.js
+++ src/components/sadness-display.js
@@ -2,9 +2,6 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 class SadnessDisplay extends React.Component{
-  constructor(props){
-    super(props)
-  }
 
   render(){
     return(
