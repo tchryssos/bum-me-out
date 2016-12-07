1788f1324eedf2bcc3ce15a265646a85a6ae842d
Troy Chryssos <github email address>
2016-12-07 14:55:20 -0500

    added some descriptive text

--- src/components/bummer-slider.js
+++ src/components/bummer-slider.js
@@ -1,20 +1,38 @@
 import React from 'react'
 import Slider from 'react-slider'
 import '../styles/slider.css'
+import * as actions from '../actions'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
 
 class BummerSlider extends React.Component{
+  constructor(){
+    super()
+    this.onSliderChange=this.onSliderChange.bind(this)
+  }
+
+  onSliderChange(value){
+    console.log(value)
+  }
+
   render(){
     return(
-      <div>
+      <div className='slider-div'>
       <Slider
       defaultValue={500}
       max={500}
       min={1}
       step={1}
+      onChange={this.onSliderChange}
       />
       </div>
     )
   }
 }
 
-export default BummerSlider
+function mapDispatchToProps(dispatch){
+   return {actions: bindActionCreators(actions, dispatch)}
+}
+
+const componentCreator = connect(null, mapDispatchToProps)
+export default componentCreator(BummerSlider)
