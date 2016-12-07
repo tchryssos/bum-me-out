a684cc9ee3096e0a1cf014c77b44977ddc51a00a
Troy Chryssos <github email address>
2016-12-07 14:08:33 -0500

    got a slider working, little bit of custom styling to make it round. That's kinda nice?

--- src/components/bummer-slider.js
+++ src/components/bummer-slider.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import ReactBootstrapSlider from 'react-bootstrap-slider'
-import '../styles/bootstrap-slider.css'
-
+import Slider from 'react-slider'
+import '../styles/slider.css'
 
 class BummerSlider extends React.Component{
   render(){
     return(
       <div>
-        <ReactBootstrapSlider
-        value={3}
-        step={1}
-        min={1}
-        max={5}/>
+      <Slider
+      defaultValue={5}
+      max={10}
+      min={0}
+      step={1}
+      />
       </div>
     )
   }
