e1f29dc0a0107234fa590681deb3fac7f43bd8a6
Troy Chryssos <github email address>
2016-12-07 13:04:06 -0500

    added 'bummer slider' component, beginning of new build

--- src/components/bummer-slider.js
+++ src/components/bummer-slider.js
@@ -1,2 +1,20 @@
 import React from 'react'
-import {Link} from 'react-router'
+import ReactBootstrapSlider from 'react-bootstrap-slider'
+import '../styles/bootstrap-slider.css'
+
+
+class BummerSlider extends React.Component{
+  render(){
+    return(
+      <div>
+        <ReactBootstrapSlider
+        value={3}
+        step={1}
+        min={1}
+        max={5}/>
+      </div>
+    )
+  }
+}
+
+export default BummerSlider
