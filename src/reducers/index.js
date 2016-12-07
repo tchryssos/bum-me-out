1e9b276836dcf85b321d88d740ff037e6e2cc186
Troy Chryssos <github email address>
2016-12-07 14:58:54 -0500

    added set sadness action and corresponding reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import fetchSadnessReducer from './set_sadness_reducer.js'
 
  const rootReducer =  combineReducers({
+   sadness: fetchSadnessReducer
  })
 
 export default rootReducer;
